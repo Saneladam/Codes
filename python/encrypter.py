5a0e2fcd7b6e37e6b7ee50b6738150bd62999910
Void User <romangarciaguill@gmail.com>
2026-05-14 23:49:10 +0200

    Initial Commit

---
+++ python/encrypter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# =============================================================================
+# Authors:      Román García Guill
+# Contact:      romangarciaguill@gmail.com
+# Created:      Wed 13. May 2026
+#
+# Purpose:     Encrypts using the XOR command 
+# =============================================================================
+
+import sysarg
+
+def encrypt(content, passphrase):
+
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
