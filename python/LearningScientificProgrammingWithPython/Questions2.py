8a8f17d37f5d152690b55212f7c33ba7840b0f96
Román <romangarciaguill@gmail.com>
2026-07-22 17:43:48 +0100

    Wed 22 Jul 17:43:48 BST 2026

---
+++ python/LearningScientificProgrammingWithPython/Questions2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# =============================================================================
+# Authors:      Román García Guill
+# Contact:      romangarciaguill@gmail.com
+# Created:      Thu 16. Jul 2026
+#
+# Purpose:     Questions about Numbers, Variables, Comparisons and Logic
+# =============================================================================
+
+
+def Q2_2_5() -> None:
+    d = 8
+    e = 2
+    from math import *
+
+    sqrt(d**e)
+
+
+def main() -> None:
+    Q2_2_5()
+    pass
+
+
+if __name__ == "__main__":
+    main()
