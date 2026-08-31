9afcd5d06ca68b008ec6283888d4c84c2e57a716
Román <romangarciaguill@gmail.com>
2026-08-31 04:08:57 +0100

    Daily backup 2026-08-31 04:08

--- web_scraper/driver.py
+++ web_scraper/driver.py
@@ -8,18 +8,32 @@
 # Purpose:      Explores the web
 # =============================================================================
 
+INTERFACE = False
+
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-# options.add_experimental_option("exludeSwitches", ["enable-automation"])
-# options.add_experimental_option("useAutomationExtension", False)
-options.add_argument("--disable-blinnk-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-extensions")
+# options.add_argument("--disable-gpu")
 
-driver = webdriver.Chrome()
+# without Interface
+if INTERFACE:
+    options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
 
 
 # driver.get("https://duckduckgo.com")
 driver.get("https://bot.sannysoft.com")
+
+print("navigator.webdriver:", driver.execute_script("return navigator.webdriver"))
+print("userAgent:", driver.execute_script("return navigator.userAgent"))
+print("platform:", driver.execute_script("return navigator.platform"))
+print("languages:", driver.execute_script("return navigator.languages"))
+print("plugins:", driver.execute_script("return navigator.plugins.length"))
 
 input("Press Enter to closethe browser ... ")
 
